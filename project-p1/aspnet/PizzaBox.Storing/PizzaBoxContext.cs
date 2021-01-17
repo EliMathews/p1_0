d0904721bbcd9065a32912ff45b8bc49b9975f7f
Eli Mathews <eli.mathews@revature.net>
2021-01-16 22:36:37 -0600

    Filled in model builder (context)

--- project-p1/aspnet/PizzaBox.Storing/PizzaBoxContext.cs
+++ project-p1/aspnet/PizzaBox.Storing/PizzaBoxContext.cs
@@ -13,13 +13,24 @@ namespace PizzaBox.Storing
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
+      builder.Entity<Customer>().HasKey(c => c.EntityId);
       builder.Entity<Order>().HasKey(o => o.EntityId); //this statement tells efcore that the order table has pk entityid
+      builder.Entity<Pizza>().HasKey(p => p.EntityId);
       builder.Entity<Store>().HasKey(s => s.EntityId);
 
+      builder.Entity<Customer>().HasData(
+        new Customer() { EntityId = 1, Name = "Bruce Wayne" },
+        new Customer() { EntityId = 2, Name = "Prince" }
+      );
+
+      builder.Entity<Pizza>().HasData(
+        new Pizza() { EntityId = 1, Name = "Cheese Pizza" },
+        new Pizza() { EntityId = 2, Name = "Pepperoni Pizza" }
+      );
+
       builder.Entity<Store>().HasData(
-        new Store() { EntityId = 1, Name = "Texas" },
-        new Store() { EntityId = 2, Name = "Maryland" },
-        new Store() { EntityId = 3, Name = "Florida" }
+        new Store() { EntityId = 1, Name = "Now That's A Pie" },
+        new Store() { EntityId = 2, Name = "The Pizza Reunion" }
       );
 
 
